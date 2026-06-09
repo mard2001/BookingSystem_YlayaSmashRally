0f5895d4fd33d82b6e3c19c77954c439b6b05137
MarvinNavarro01 <18103694@usc.edu.ph>
2026-06-09 13:14:52 +0800

    Modifying for production.

--- Server/src/connect.js
+++ Server/src/connect.js
@@ -1,8 +1,8 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 import 'dotenv/config';
 
 // DEVELOPMENT
-// export const db = mysql.createConnection({
+// export const db = mysql.createPool({
 //     host: process.env.DB_HOST,
 //     user: process.env.DB_USER,
 //     password: process.env.DB_PASSWORD,
@@ -12,4 +12,4 @@ import 'dotenv/config';
 
 // PRODUCTION
 const urlDB = `mysql://root:XhqEgDZGPOORDQuzcEbjCwVYbZPSGCfp@mysql.railway.internal:3306/railway`;
-export const db = mysql.createConnection(urlDB);
+export const db = mysql.createPool(urlDB);
