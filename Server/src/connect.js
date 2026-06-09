242fce09bcab8d214d002f3b00db665e50eee429
MarvinNavarro01 <18103694@usc.edu.ph>
2026-06-09 20:00:21 +0800

    Modifying for production.

--- Server/src/connect.js
+++ Server/src/connect.js
@@ -2,17 +2,18 @@ import mysql from "mysql2";
 import 'dotenv/config';
 
 // DEVELOPMENT
-// export const db = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// })
+export const db = mysql.createPool({
+    host: process.env.NODE_STAT == "PRODUCTION"? process.env.PROD_DB_HOST : process.env.DB_HOST,
+    port: process.env.NODE_STAT == "PRODUCTION" ? process.env.PROD_DB_PORT : process.env.DB_PORT,
+    user: process.env.NODE_STAT == "PRODUCTION"? process.env.PROD_DB_USER : process.env.DB_USER,
+    password: process.env.NODE_STAT == "PRODUCTION"? process.env.PROD_DB_PASSWORD : process.env.DB_PASSWORD,
+    database: process.env.NODE_STAT == "PRODUCTION"? process.env.PROD_DB_DATABASE : process.env.DB_DATABASE
+})
 
 
 // PRODUCTION
-const urlDB = `mysql://root:XhqEgDZGPOORDQuzcEbjCwVYbZPSGCfp@mysql.railway.internal:3306/railway`;
-export const db = mysql.createPool(urlDB);
+// const urlDB = `mysql://root:XhqEgDZGPOORDQuzcEbjCwVYbZPSGCfp@mysql.railway.internal:3306/railway`;
+// export const db = mysql.createPool(urlDB);
 
 export const getConnection = () => {
     return new Promise((resolve, reject) => {
