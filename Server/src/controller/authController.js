5a997d37cd448d07864a6ddc0da513dd84c2606d
MarvinNavarro01 <18103694@usc.edu.ph>
2026-06-09 13:41:09 +0800

    Modifying for production.

--- Server/src/controller/authController.js
+++ Server/src/controller/authController.js
@@ -205,17 +205,15 @@ export const login = (req, res) => {
 
             res.cookie('accessToken', accessToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-                sameSite: 'strict',
+                secure: true,
+                sameSite: 'None',
                 maxAge: 15 * 60 * 1000           // 15 minutes
             });
 
             res.cookie('refreshToken', refreshToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-                sameSite: 'strict',
+                secure: true,
+                sameSite: 'None',
                 maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
             });
 
